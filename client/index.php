7422b653c95c8b2e8529955201dcc29c41f86055
Nicolas 123 <100376064+Nicolastafur@users.noreply.github.com>
2022-09-20 15:27:41 -0500

    Add files via upload

--- client/index.php
+++ client/index.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-    
+    <link rel="stylesheet" href="../styles/clientes.css">
     <title>Clientes</title>
 </head>
 <body>
@@ -57,13 +57,13 @@
                 contenido.innerHTML += `
                 
                 <tr>
-                    <th scope="row">${ valor.id_cliente }</th>
+                    <td scope="row">${ valor.id_cliente }</td>
                     <td>${ valor.nombre_cliente }</td>
                     <td>${ valor.apellido_cliente }</td>
                     <td>${ valor.direccion  }</td>
                     <td>${ valor.celular  }</td>
-                    <td> <a href="./insertar.php">borrar</a></td>
-                    <td> <a href="./insertar.php">modificar</a></td>
+                    <td><a id="q1" href="./insertar.php">borrar</a></td>
+                    <td> <a id="q2" href="./insertar.php">modificar</a></td>
                 </tr>
                 
                 `
@@ -72,8 +72,15 @@
             
         
 
-    </script>
-        </div>
+        </script>
+        </table class="dos">
+            <td>
+                <a href="../admin/index.php" onmouseup="window.close()">
+                    <input  id="bot" type="button" value="cerrar" name="cerrar" >
+                </a>
+            </td>
+        </table>
+    </div>
     </form>
 </body>
 </html>
--- client/jyson.php
+++ client/jyson.php
@@ -2,29 +2,29 @@
     include_once("./client.php");
 
 
-class Apicliente{
-    function getAll(){
-        $cliente=new cliente();
-        $clientes=array();
-        $clientes['cliente']=array();
+    class Apicliente{
+        function getAll(){
+            $cliente=new cliente();
+            $clientes=array();
+            $clientes['cliente']=array();
 
-        $pes=$cliente->Getcliente();
-        if ($pes->rowCount()){
-            while ($ca=$pes->fetch(PDO::FETCH_ASSOC)){
-                $item2=array(
-                    "id_cliente"=>$ca['id_cliente'],
-                    "nombre_cliente"=>$ca['nombre_cliente'],
-                    "apellido_cliente"=>$ca['apellido_cliente'],
-                    "direccion"=>$ca['direccion'],
-                    "celular"=>$ca['celular'],
-                );
-                array_push($clientes["cliente"],$item2);
+            $pes=$cliente->Getcliente();
+            if ($pes->rowCount()){
+                while ($ca=$pes->fetch(PDO::FETCH_ASSOC)){
+                    $item2=array(
+                        "id_cliente"=>$ca['id_cliente'],
+                        "nombre_cliente"=>$ca['nombre_cliente'],
+                        "apellido_cliente"=>$ca['apellido_cliente'],
+                        "direccion"=>$ca['direccion'],
+                        "celular"=>$ca['celular'],
+                    );
+                    array_push($clientes["cliente"],$item2);
+                }
+                echo json_encode($clientes);
             }
-            echo json_encode($clientes);
-        }
-        else{
-            echo json_encode(array("mensaje"=>'no hay elementos'));
+            else{
+                echo json_encode(array("mensaje"=>'no hay elementos'));
+            }
         }
     }
-}
 ?>
