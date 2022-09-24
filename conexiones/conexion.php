01f3214e7c76d7d54f150cd0df8999a6d03af8c1
Gat0_o <74437881+antonig5@users.noreply.github.com>
2022-09-24 18:57:03 -0500

    Add files via upload

--- conexiones/conexion.php
+++ conexiones/conexion.php
@@ -1,16 +1,13 @@
 <?php
-// datos de la database
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$Database = "spacanino";
-
-$mysqli = new mysqli ($hostname, $username, $password, $Database);
-
-//conectividad
-if ($mysqli -> connect_errno) {
-
-    die("fallo la conexion" . mysqli_connect_errno());
+$contraseña = "12345678";
+$usuario = "root";
+$nombre_base_de_datos = "spacaninos";
+try {
+    $base_de_datos = new PDO('mysql:host=localhost;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
+    $base_de_datos->query("set names utf8;");
+    $base_de_datos->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
+    $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $base_de_datos->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+} catch (Exception $e) {
+    echo "Ocurrió algo con la base de datos: " . $e->getMessage();
 }
-
-?>
